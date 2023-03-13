f91b18788e649bc81a61ad8c458b5caed51a9056
Filipe Silva <filipeqs@outlook.com>
2023-03-12 21:26:50 -0400

    publishing and subscribing to events

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -4,19 +4,19 @@ console.clear();
 
 // stan = client
 const stan = nats.connect('ticketing', 'abc', {
-    url: 'http://localhost:4222',
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
-    console.log('publisher connected to NATS');
+  console.log('publisher connected to NATS');
 
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20,
-    });
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
 
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    });
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
 });
